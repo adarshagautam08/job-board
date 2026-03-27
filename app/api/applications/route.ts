74d4ea080b7b56d14f25f76ef374646b143738d7
Adarsha Gautam <gautamadarsha08@gmail.com>
2026-03-27 10:37:47 +0545

    fix cookie name in my-jobs and applicants API routes

--- app/api/applications/route.ts
+++ app/api/applications/route.ts
@@ -51,32 +51,62 @@ export  async function POST(request:NextRequest)
 
 
 
+// export async function GET(request:NextRequest) {
+//     const token=await getToken({
+//         req:request,
+//         secret:process.env.NEXTAUTH_SECRET
+//     })
+//     if(!token)
+//     {
+//         return NextResponse.json(
+//             {error:'unauthorized'},
+//             {status:400}
+//         )
+//     }
+//     const seekerId=token.sub
+//     const application=await prisma.application.findMany(
+//         {
+//             where:{seekerId:seekerId as  string },
+//             include:{
+//                 job:true
+//             },
+//             orderBy:{
+//                 createdAt:'desc'
+//             }
+
+//         }
+//     )
+//     return NextResponse.json(application)
+
+
+// }
+
 export async function GET(request:NextRequest) {
-    const token=await getToken({
-        req:request,
-        secret:process.env.NEXTAUTH_SECRET
+    const token = await getToken({
+        req: request,
+        secret: process.env.NEXTAUTH_SECRET,
+        cookieName: process.env.NODE_ENV === "production"
+            ? "__Secure-next-auth.session-token"
+            : "next-auth.session-token"
     })
     if(!token)
     {
         return NextResponse.json(
             {error:'unauthorized'},
-            {status:400}
+            {status:401}
         )
     }
     const seekerId=token.sub
     const application=await prisma.application.findMany(
         {
-            where:{seekerId:seekerId as  string },
+            where:{seekerId:seekerId as string},
             include:{
                 job:true
             },
             orderBy:{
                 createdAt:'desc'
             }
-
         }
     )
     return NextResponse.json(application)
-
-
 }
